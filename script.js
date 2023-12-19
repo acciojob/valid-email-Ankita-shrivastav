d5f240a800b705d5bc708476651dc8a61354b546
Ankita-shrivastav <93003257+Ankita-shrivastav@users.noreply.github.com>
2023-12-20 03:18:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 function validEmail(str) {
   //your JS code here.
+	var pattern=/^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+	return pattern.test(email);
+	console.log(validEmail('abc@example.com'));
+	console.log(validEmail('xyz@abc.com.in'));
+	console.log(validEmail('john.doe@com.'));
+	console.log(validEmail('@example.com'));
+	console.log(validEmail('') );
 }
 
+
+
 // Do not change the code below.
-const str = prompt("Enter an email address.");
-alert(validEmail(str));
+// const str = prompt("Enter an email address.");
+// alert(validEmail(str));
